227fc29f236635109c770da136962fedddf52e91
pdp <john.lgd65@gmail.com>
2023-03-03 10:20:03 +0500

    ignore done in advanced way

--- src/main/java/uz/pdp/springframeworkcore/Main.java
+++ src/main/java/uz/pdp/springframeworkcore/Main.java
@@ -2,6 +2,6 @@ package uz.pdp.springframeworkcore;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello PDP!");
     }
 }
