e7d5e527adf2df3b6c6706fdd81970a9629733e0
pdp <john.lgd65@gmail.com>
2023-03-13 04:37:12 +0500

    thmeleaf-features done

--- src/main/java/uz/pdp/springframeworkcore/controller/HomeController.java
+++ src/main/java/uz/pdp/springframeworkcore/controller/HomeController.java
@@ -1,26 +1,26 @@
 package uz.pdp.springframeworkcore.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import uz.pdp.springframeworkcore.config.SessionUser;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Locale;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final MessageSource messageSource;
 
-    private final SessionUser sessionUser;
 
 
     @GetMapping("/home")
-    public String homePage() {
-        Locale locale = sessionUser.getLocale();
-        String message = messageSource.getMessage("welcome3", new Object[]{"Javohir"}, locale);
+    public String homePage(@CookieValue String language, @RequestParam String lang) {
+        lang = Objects.requireNonNullElse(lang, language);
+        String message = messageSource.getMessage("welcome3", new Object[]{"Javohir"}, Locale.forLanguageTag(lang));
         System.out.println("message = " + message);
         return "home";
     }
